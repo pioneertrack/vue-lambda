c2fd5517f7c0faf5b9cfb236e363f449369605c4
Steven Motley <stevenmotley89@gmail.com>
2018-03-27 16:57:41 -0400

    Update to config

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   API_ENDPOINT:'"https://api.app.weathercheck.co"',
   DOMAIN:'"api.app.weathercheck.co"',
-  COGNITO_REGION: '"us-east-2',
+  COGNITO_REGION: '"us-east-2"',
   COGNITO_USERPOOLID: '"us-east-2_ZncmElBRt"',
   COGNITO_CLIENTID: '"rapb4b90d5tfjst0f9i56569d"',
   COGNITO_IDENTITYPOOLID: '"us-east-2:97392044-5732-43f3-aac1-348a9574cb40"',
