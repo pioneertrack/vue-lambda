850b02857033a00226f1c62c55c604d97e86fd8d
Steven Motley <stevenmotley89@gmail.com>
2018-03-27 20:59:25 -0400

    Update env variables.

--- config/prod.env.js
+++ config/prod.env.js
@@ -7,4 +7,5 @@ module.exports = {
   COGNITO_USERPOOLID: '"us-east-2_ZncmElBRt"',
   COGNITO_CLIENTID: '"rapb4b90d5tfjst0f9i56569d"',
   COGNITO_IDENTITYPOOLID: '"us-east-2:97392044-5732-43f3-aac1-348a9574cb40"',
+  SS_AUTHID:'"6413738716745430"',
 }
--- config/staging.env.js
+++ config/staging.env.js
@@ -7,4 +7,5 @@ module.exports = {
   COGNITO_USERPOOLID: '"us-east-1_5pGfdfJl2"',
   COGNITO_CLIENTID: '"4sm7eloi2h1vctbl9kt9iaunja"',
   COGNITO_IDENTITYPOOLID: '"us-east-1:247ccaf8-116f-4bba-b77c-cbdafa451ac5"',
+  SS_AUTHID:'"6413738716745430"',
 }
